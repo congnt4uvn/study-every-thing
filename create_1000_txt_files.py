aba8d8e1a476700d54978439e3c65110627e8370
Congllspkt <thanhcongnguyen1995@gmail.com>
2026-07-17 22:45:42 +0700

    xxx

--- create_1000_txt_files.py
+++
@@ -1,7 +0,0 @@
-import pathlib
-
-folder = pathlib.Path('files_txt')
-folder.mkdir(parents=True, exist_ok=True)
-for i in range(1, 1000):
-    path = folder / f"{i:03}.txt"
-    path.write_text("")  # create empty file
--- push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd .\study-every-thing\
-git add .
-git commit -m "XXX"
-git push
